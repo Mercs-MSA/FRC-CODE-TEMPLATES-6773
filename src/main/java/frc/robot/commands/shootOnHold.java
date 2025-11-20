e796b7dcb572385b2a7e1bef1983967910750283
lawrencetanFTC <lawrencetan458@gmail.com>
2025-11-19 20:27:16 -0600

    commit

--- src/main/java/frc/robot/commands/shootOnHold.java
+++ src/main/java/frc/robot/commands/shootOnHold.java
@@ -2,14 +2,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.GenericShooter;
 
 class shootCommand extends Command {
 
     private GenericShooter m_subsystem;
-    long time = System.currentTimeMillis();
-        private XboxController controller;
         
         @Override
         public boolean isFinished() {
@@ -19,7 +16,6 @@ class shootCommand extends Command {
         @Override
         public void initialize() {
             m_subsystem.speedUp();
-            time = System.currentTimeMillis();
         }
     
     
@@ -31,7 +27,6 @@ class shootCommand extends Command {
         shootCommand(GenericShooter shooter, XboxController xboxController) {
             addRequirements(shooter);
             this.m_subsystem = shooter;
-            this.controller = xboxController;
-    }
+        }
 
 }
