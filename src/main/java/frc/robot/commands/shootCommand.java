849113b05226efa5c2541cedc25438e63471722c
OutcastsRobotics <communityroboticsboosters@gmail.com>
2025-11-19 19:56:31 -0600

    shooter

---
+++ src/main/java/frc/robot/commands/shootCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.GenericShooter;
+
+class shootCommand extends Command {
+
+    private GenericShooter m_subsystem;
+    long time = System.currentTimeMillis();
+    
+    @Override
+    public boolean isFinished() {
+        return time + ShooterConstants.shootTime >= System.currentTimeMillis();
+    }
+
+    @Override
+    public void initialize() {
+        m_subsystem.speedUp();
+        time = System.currentTimeMillis();
+    }
+
+
+    @Override
+    public void end(boolean interrupted){
+        m_subsystem.stop();
+    }
+
+    shootCommand(GenericShooter shooter) {
+        addRequirements(shooter);
+        this.m_subsystem = shooter;
+    }
+
+}
